486b9aa71aa010507f01740cc64a15168f5140bf
alex <apiggott@ikanow.com>
2015-03-29 23:12:07 -0400

    Added mockito libs

--- test/demo/elasticinsight_manager/server/MockMongoServer.java
+++ test/demo/elasticinsight_manager/server/MockMongoServer.java
@@ -1,5 +1,7 @@
 package demo.elasticinsight_manager.server;
 
+import org.mockito.Mockito;
+
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBCursor;
 
@@ -7,11 +9,18 @@ import demo.elasticinsight_manager.services.DatastoreService;
 
 public class MockMongoServer implements DatastoreService {
 
+	protected DBCursor _op_log_cursor = null;
+	
 	@Override
 	public DBCursor getOperationsCursor(BasicDBObject query) {
-		// TODO 
+		if (null == _op_log_cursor) {
+			_op_log_cursor = Mockito.mock(DBCursor.class);
+			Mockito.when(_op_log_cursor.hasNext()).thenReturn(false);
+			Mockito.when(_op_log_cursor.getCursorId()).thenReturn(0L);
+		}
+		
 		System.out.println("getOperationsCursor: " + query);
-		return null;
+		return _op_log_cursor;
 	}
 
 }
